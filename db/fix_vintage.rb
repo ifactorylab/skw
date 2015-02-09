279945b82c50f6c246a54daa9ec941a0e5847683
minsikzzang <minsikzzang@gmail.com>
2015-02-09 22:10:13 +0000

    db:fix_vintage added

--- db/fix_vintage.rb
+++ db/fix_vintage.rb
@@ -1,19 +1,17 @@
 def get_year(yy)
   if yy > 20
     yy + 1900
-  elsif yy <= 20 and yy > 0
+  elsif yy <= 20
     yy + 2000
-  else
-    0
   end
 end
 
 Item.all.each do |i|
   item = i.name.split(" - ");
-  yy = get_year(item[1].to_i)
-  puts "#{item[0]}, #{item[1]}, #{yy}"
+  if item.count > 1
+    yy = get_year(item[1].to_i)
+    puts "#{item[0]}, #{item[1]}, #{yy}"
 
-  if yy > 0
     vintage = ItemVintage.where(year: yy).first
     i.vintage_id = vintage.id
     i.name = item[0]
